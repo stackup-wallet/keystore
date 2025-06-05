5b54c33ac0bfc354cbc842e58597aa6dc0fe449c
hazim <hazim.jumali@gmail.com>
2025-06-05 15:07:06 +1000

    Update nonce mechanism (#2)

--- src/interface/IKeystore.sol
+++ src/interface/IKeystore.sol
@@ -4,12 +4,14 @@ pragma solidity ^0.8.13;
 import {UpdateAction, ValidateAction} from "./actions.sol";
 
 interface IKeystore {
-    error InvalidNextHash();
+    error InvalidNonce();
     error InvalidProof();
     error InvalidNode();
     error InvalidVerifier();
 
-    event RootHashUpdated(bytes32 indexed refHash, bytes32 oldRoot, bytes32 newRoot, bool success);
+    event RootHashUpdated(
+        bytes32 indexed refHash, bytes32 nextHash, uint256 nonce, bytes32[] proof, bytes node, bytes data, bool success
+    );
 
     function handleUpdates(UpdateAction[] calldata actions) external;
     function validate(ValidateAction calldata action) external view returns (bool);
